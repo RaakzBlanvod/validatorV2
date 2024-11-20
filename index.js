7f0ca851d661a7c5c1e74316b9dd38483e34887c
RaakzBlanvod <icky6119@gmail.com>
2024-11-20 11:16:47 +0300

    task 1 Done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+import EmailValidator from './src/EmailSchema.js';
 
+class Validator {
+  email() {
+    return new EmailValidator();
+  }
+}
+
+export default Validator;
--- src/EmailSchema.js
+++ src/EmailSchema.js
@@ -1,1 +1,11 @@
+class EmailValidator {
+  isEmail(str) {
+    return typeof str === 'string' && str.includes('@');
+  }
 
+  isValid(str) {
+    return this.isEmail(str);
+  }
+}
+
+export default EmailValidator;
